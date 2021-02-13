be485c1f62a816602cb723e9269adc5150a3c55b
Your Name <you@example.com>
2021-02-13 20:02:29 +0800

    remove any files

--- Python_segmatation/dpf/MobileNet_Unet/mytest.py
+++
@@ -1,9 +0,0 @@
-import cv2
-import numpy as np
-import matplotlib.pyplot as plt
-path = r'C:\Users\86135\Desktop\train10000\datas\train10000\train_label/423190.png'
-img = cv2.imread(path, -1)
-print(img.shape)
-print(img.dtype)
-plt.imshow(img*50)
-plt.show()
--- Python_segmatation/dpf/multi_unet/test.py
+++
@@ -1,9 +0,0 @@
-import cv2
-import numpy as np
-import matplotlib.pyplot as plt
-from data import *
-from model import *
-classNum = 8
-test_label_path = '/home/ymh/桌面/U-net/data/test_image'
-
-
